569a106e18dfc6aece8993ab8f880cc6f7d5c6eb
Weng Long Pang <weng-pang@users.noreply.github.com>
2024-01-03 18:46:23 +0800

    Adjust width

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,10 @@ export default function Home() {
 
       <main>
         <Header title="OAS 網絡維護 Network maintenance " />
-        <p className="description">
+        <p className="description" style="width:70%;">
           2024年1月3日晚上10點至翌日6點半進行網絡維護，在維護期間，網上報名系統OAS會受到中斷的影響，不便之處，敬請見諒。
         </p>
-        <p className="description">
+        <p className="description" style="width:70%;">
         Network maintenance will be carried out from 10:00 PM on January 3, 2024, until 6:30 AM the following day.  During the maintenance period, the Online Application System (OAS) will be temporarily unavailable.  We apologize for any inconvenience caused.
         </p>
         ::CLOUDFLARE_ERROR_500S_BOX::
