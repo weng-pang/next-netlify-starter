af98e8cf87f957d70f4fe0e47b42c146c2a66041
Weng Long Pang <weng-pang@users.noreply.github.com>
2024-01-03 18:42:27 +0800

    Apply maintenance message

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,19 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>OAS 網絡維護 Network maintenance</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="OAS 網絡維護 Network maintenance " />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          2024年1月3日晚上10點至翌日6點半進行網絡維護，在維護期間，網上報名系統OAS會受到中斷的影響，不便之處，敬請見諒。
+        </p>
+        <p className="description">
+        Network maintenance will be carried out from 10:00 PM on January 3, 2024, until 6:30 AM the following day.  During the maintenance period, the Online Application System (OAS) will be temporarily unavailable.  We apologize for any inconvenience caused.
         </p>
       </main>
 
-      <Footer />
     </div>
   )
 }
